b1384c2050b1c2873d60b11614029690de5c02b2
EsauM10 <esau2096@gmail.com>
2023-06-18 13:45:55 -0300

    build: add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="fogbesu",
+    version="0.1.0",
+    description='Plugin to build a private Besu blockchain in Fogbed.',
+    long_description='Plugin to build a private Besu blockchain in Fogbed.',
+    keywords=['networking', 'emulator', 'protocol', 'Internet', 'dlt', 'besu', 'fog'],
+    url='https://github.com/EsauM10/fogbesu',
+    author='Esaú Mascarenhas',
+    author_email='esaumasc@gmail.com',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python 3.8',
+        'Topic :: System :: Emulators'
+        'Operating System :: Ubunbu OS',
+        'Blockchain :: Hyperledger Besu',
+    ],
+    install_requires = [
+        'fogbed @ https://github.com/EsauM10/fogbed',
+    ],
+    packages=find_packages(),
+    zip_safe=False
+)
